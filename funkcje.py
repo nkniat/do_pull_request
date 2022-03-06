3f51315deaf3c46d985beaaffd2244124730d999
Darek <darek@no.replay.github.com>
2022-03-06 11:07:58 +0100

    dodane funkcje.py

--- funkcje.py
+++ funkcje.py
@@ -2,6 +2,7 @@ def usun_puste(content):
     content = str(content[0])
     x = content.replace(" ", "")
     return x
+
 def ile_a(content):
     char = str('a')
     content = str(content[0])
@@ -11,4 +12,11 @@ def ile_a(content):
             count += 1
     return count
 
-
+def ile(content, x):
+    char = str(x)
+    content = str(content[0])
+    count = 0
+    for c in content:
+        if char == c:
+            count += 1
+    return count
